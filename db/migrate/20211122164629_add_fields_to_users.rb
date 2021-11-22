11f426ecb8362c7d0f182384d16cd96a51f1f726
Guenole Quideau <gqprof@protonmail.com>
2021-11-22 21:46:10 +0100

    Migration OK

--- db/migrate/20211122164629_add_fields_to_users.rb
+++ db/migrate/20211122164629_add_fields_to_users.rb
@@ -1,11 +1,11 @@
 class AddFieldsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :first_name, :string
-    add_column :last_name, :string
-    add_column :birth_date, :date
-    add_column :bio, :text
-    add_column :role, :string
-    add_column :address, :string
-    add_column :phone_number, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :birth_date, :date
+    add_column :users, :bio, :text
+    add_column :users, :role, :string
+    add_column :users, :address, :string
+    add_column :users, :phone_number, :string
   end
 end
