ec11436d8614caa927333963be6db41f4b1dd0c4
Guenole Quideau <gqprof@protonmail.com>
2021-11-23 10:59:43 +0100

    Change pet column

--- db/migrate/20211123093811_update_pet_friendly_to_swimming_pool.rb
+++ db/migrate/20211123093811_update_pet_friendly_to_swimming_pool.rb
@@ -1,7 +1,5 @@
 class UpdatePetFriendlyToSwimmingPool < ActiveRecord::Migration[6.0]
   def change
-    change_table :swimming_pools do |t|
-      t.change :pets_friendly_boolean, :boolean
-    end
+    rename_column :swimming_pools, :pets_friendly_boolean, :pets_friendly
   end
 end
