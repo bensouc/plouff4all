2a7a1577896cc004ebf1cccbd6acd6b178b61342
Guenole Quideau <gqprof@protonmail.com>
2021-11-23 10:54:15 +0100

    Abandon modif type pet_friendly

--- db/migrate/20211123093811_update_pet_friendly_to_swimming_pool.rb
+++ db/migrate/20211123093811_update_pet_friendly_to_swimming_pool.rb
@@ -1,5 +1,7 @@
 class UpdatePetFriendlyToSwimmingPool < ActiveRecord::Migration[6.0]
   def change
-    t.change :pets_friendly_boolean, :boolean
+    change_table :swimming_pools do |t|
+      t.change :pets_friendly_boolean, :boolean
+    end
   end
 end
