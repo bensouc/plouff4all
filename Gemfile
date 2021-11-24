452caa36d2f6da82f913978ac0676f3c7c5d9994
Guenole Quideau <gqprof@protonmail.com>
2021-11-24 14:56:54 +0100

    geocoder beginning

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
+gem 'geocoder'
 gem 'simple_form'
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_112117) do
+ActiveRecord::Schema.define(version: 2021_11_24_133150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 2021_11_23_112117) do
     t.string "pets_friendly"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_swimming_pools_on_user_id"
   end
 
