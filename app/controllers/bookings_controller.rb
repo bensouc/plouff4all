bec68703cf5b04bf3eda6c4675ebd89dd90e6ee2
benoit moret <benoit.moret@gmail.com>
2021-11-25 13:13:03 +0100

    add view booking dans dashboard"

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,31 @@
 class BookingsController < ApplicationController
+  def index
+    @booking = current_user.bookings_as_renter
+  end
+
+  def new
+    @new_booking = Booking.new
+    @swimming_pool = SwimmingPool.find(params[:swimming_pool_id])
+  end
+
+  def create
+    #if current_user
+      @new_booking = Booking.new(booking_params)
+      @new_booking.status = "pending"
+      @new_booking.user = current_user
+      @new_booking.swimming_pool = SwimmingPool.find(params[:swimming_pool_id])
+      @swimming_pool = SwimmingPool.find(params[:swimming_pool_id])
+      @new_booking.save
+      redirect_to dashboard_path
+      # @swimming_pool = SwimmingPool.find(params[:swimming_pool_id])
+    #else
+      #path vers login
+    #end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
+  end
 end
