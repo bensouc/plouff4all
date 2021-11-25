a46b9d966bf2e7ce1eb8c1b9f91d753b67b70590
Guenole Quideau <gqprof@protonmail.com>
2021-11-25 16:32:42 +0100

    Form for new

--- app/controllers/owner/swimming_pools_controller.rb
+++ app/controllers/owner/swimming_pools_controller.rb
@@ -2,4 +2,8 @@ class Owner::SwimmingPoolsController < ApplicationController
   def index
     @swimming_pools = current_user.swimming_pools
   end
+
+  def new
+    @swimming_pool = SwimmingPool.new
+  end
 end
